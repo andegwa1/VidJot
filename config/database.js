99a267c067fae46e7e7171de8da1872e1ae6f854
andegwa1 <andegwa1@student.gsu.edu>
2017-12-18 14:17:05 -0500

    ENV

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-if(process.env.NODE_ENC == 'production'){
+if(process.env.NODE_ENV == 'production'){
     module.exports = {mongoURI: 'mongodb://al:kn5659@ds159926.mlab.com:59926/vidjot-prod'}
 } else {
     module.exports = {mongoURI: 'mongodb://localhost/vidjot-dev'}
